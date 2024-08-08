b26ac1b01c222df541b235c05d7b7ba7461c7d30
Diogo <diogo.araujoctt@gmail.com>
2024-08-08 17:53:02 -0300

    outras alterações

--- conftest.py
+++ conftest.py
@@ -1,15 +1,14 @@
 import pytest
+import time
 from selenium import webdriver
 
 @pytest.fixture(scope="module")
+
 def browser():
     chrome_options = webdriver.ChromeOptions()
+    
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--remote-debugging-port=9222')
-    chrome_options.binary_location = '/usr/bin/google-chrome'  # Caminho para o binário do Chrome
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
--- tests/test_home.py
+++ tests/test_home.py
@@ -1,8 +1,7 @@
 import time
 
 def testar_home(browser):
-	url = 'http:127.0.0.1:8081'
-	browser.get(url)
-	time.sleep(10)
+	browser.get('http://127.0.0.1:8081')
+	time.sleep(5)
 	assert browser.title == "Dash"
 	print("Página da home testada com sucesso!")
