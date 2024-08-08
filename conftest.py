a86e188935754dde5a65a217b03582990e7bb95f
Diogo <diogo.araujoctt@gmail.com>
2024-08-08 17:27:39 -0300

    problema no chrome

--- conftest.py
+++ conftest.py
@@ -9,6 +9,7 @@ def browser():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--remote-debugging-port=9222')
+    chrome_options.binary_location = '/usr/bin/google-chrome'  # Caminho para o binário do Chrome
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
