6fa1d2b423b807fcfb93678b19d5d7f7b5e1c1f2
Diogo <diogo.araujoctt@gmail.com>
2024-08-08 17:14:37 -0300

    problema no chrome
    '
    '
    '
    
    exirt
    exit
    e'
    '

--- conftest.py
+++ conftest.py
@@ -1,14 +1,14 @@
 import pytest
-import time
 from selenium import webdriver
 
 @pytest.fixture(scope="module")
-
 def browser():
-	chrome_options = webdriver.ChromeOptions()
-	chrome_options.add_argument('--headless')
-	chrome_options.add_argument('--disable-gpu')
-	chrome_options.add_argument('--no-sandbox')
-	driver = webdriver.Chrome(options=chrome_options)
-	yield driver
-	driver.quit()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    driver = webdriver.Chrome(options=chrome_options)
+    yield driver
+    driver.quit()
